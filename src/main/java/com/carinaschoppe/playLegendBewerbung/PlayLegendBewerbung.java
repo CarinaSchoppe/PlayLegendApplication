d97d610df3ae7e7706dffc3c41627934218748bd
Carina Sophie Schoppe <carina.schoppe2912@gmail.com>
2024-09-05 11:28:07 +0200

    first commit

---
+++ src/main/java/com/carinaschoppe/playLegendBewerbung/PlayLegendBewerbung.java
@@ -0,0 +1,17 @@
+package com.carinaschoppe.playLegendBewerbung;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayLegendBewerbung extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
